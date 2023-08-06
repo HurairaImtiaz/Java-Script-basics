398041dea5a5c1abe9aa843f9bc6c1f8777fc831
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2023-08-06 18:12:42 +0500

    updated

--- backTick.js
+++ backTick.js
@@ -1,7 +1,8 @@
 // bac-tick in a string 
-let fname = 'huraira';
-let lname = ' imtiaz';
-let age = 21;
-let school = 'DPS'
-console.log(`the name of student is ${fname} ${lname}. His age is ${age}.`);
+// let fname = 'huraira';
+// let lname = ' imtiaz';
+// let age = 21;
+// let school = 'DPS'
+// console.log(`the name of student is ${fname} ${lname}. His age is ${age}.`);
+ 
 
