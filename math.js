acce2de4b093deaab695c2fcd5c76f920be986f0
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2023-08-06 18:14:12 +0500

    Matth.js
    math functions

---
+++ math.js
@@ -0,0 +1,9 @@
+Math.PI // 3.141592653589793
+//Rounding a number:
+Math.round(4.9) //5
+//Absolute value:
+Math.abs(-456) //456
+//Raises 2 to the 5th power:
+Math.pow(2,5) //32
+//Removes decimal:
+Math.floor(3.9999) //3
